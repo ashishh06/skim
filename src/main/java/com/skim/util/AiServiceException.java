ec90e14c696e9ad7cf1432d3829e51ccd2e62261
Ashish Sharma <ashishhariom14@gmail.com>
2026-09-02 22:54:41 +0530

    added the api key vafriable and added service exception

---
+++ src/main/java/com/skim/util/AiServiceException.java
@@ -0,0 +1,7 @@
+package com.skim.util;
+
+public class AiServiceException extends RuntimeException {
+    public AiServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
