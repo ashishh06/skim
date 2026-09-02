f0fd991f7b039284c58ca681d328dc5e9cad5802
Ashish Sharma <ashishhariom14@gmail.com>
2026-09-02 19:28:54 +0530

    initial commit

---
+++ src/main/java/com/skim/SkimApplication.java
@@ -0,0 +1,13 @@
+package com.skim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skim/SkimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
